b3de4d0d12178e1840f9f90943eb7a128966fc12
BenetAlex <37636490+BenetAlex@users.noreply.github.com>
2018-03-22 13:27:56 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const PREFIX = "-";
-const low = require('lowdb')
-const FileSync = require("lowdb/adapters/fileSync")
-
-const adapter = new FileSync("database.json");
-const dp = low(adapter);
-db.defaults({ histoire: [], xp: []}).write
 
   bot.on('guildMemberAdd', member => {
   bot.channels.get  ("423910982373343233").send("salut à "+ member.displayName + "  Sur le serveur Dev");
@@ -32,36 +26,12 @@ bot.on('message', message => {
             message.author.createDM().then(channel =>{
               channel.send("test message private")
             })
-            var msgauthor = message.author.id;
-
-    if(message.author.bot)return;
-    if(db.get("xp").find({user: msgauthor}).value()){
-        db.get("xp").push({user: msgauthor, xp : 1}).write();
-
-    }else{
-      var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
-      console.log(userxpdb);
-      var userxp = object.values(userxpdb)
-      console.log(userxp)
-      console.log('xp:  ${userxp[1]}')
-      db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-
-if (message.content === PREFIX + "xp"){
-  var xp = db.get("xp").filter({user: msgauthor}).find("xp").value()
-  var xpfinal = object.values(xp);
-  var xp_embed = new Discord.RichEmbed()
-  .setTitle('stat des xp de ${message.getauthor.username}')
-  .setColor('#F4D03F')
-  .setDescription('Affichage des xp')
-  .addField("XP:", '${xpfinal[1]} xp')
-  .setFooter("Voilà :p ")
-  message.channel.send({embed: xp_embed});  
-}
-
-    }
+               
 
           }
      }
 });
 
 bot.login(process.env.TOKEN);
+
+
